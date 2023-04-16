81f2ba4ef01b61221ac0a499bb999fd8a588ce6b
TurtlePower <agnkvdm@gmail.com>
2023-04-16 18:07:35 +0300

    add countries searching and markup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,82 @@
 import './css/styles.css';
+import Notify from 'notiflix';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+const refs = {
+  inputSearch: document.querySelector('input#search-box'),
+  countriesList: document.querySelector('ul.country-list'),
+  countryInfo: document.querySelector('div.country-info'),
+};
+
+refs.inputSearch.addEventListener(
+  'input',
+  debounce(onSearchInput, DEBOUNCE_DELAY)
+);
+
+function onSearchInput(e) {
+  const nameToSearch = e.target.value.trim();
+  refs.countriesList.innerHTML = '';
+  refs.countryInfo.innerHTML = '';
+
+  // если поле поиска пустое или очистили
+  if (nameToSearch === '') {
+    return;
+  }
+
+  // достаём список стран по имени из строки поиска
+  fetchCountries(nameToSearch)
+    .then(country => {
+      console.log(country);
+      // рисуем инфу о стране если нашло одну страну
+      if (country.length === 1) {
+        refs.countryInfo.innerHTML = makeCountryMarkup(country);
+      }
+
+      // рисуем список стран если нашло больше одной страны
+      if (country.length > 1 && country.length <= 10) {
+        refs.countriesList.innerHTML = makeCountriesMarkup(country);
+      }
+
+      // уведомление если стран больше 10ти
+      if (country.length > 10) {
+        showTooManyCountriesMessage();
+      }
+    })
+    // уведомление если страну не получилось найти
+    .catch(error => {
+      if (error.message === '404') {
+        showErrorMessage();
+        return;
+      }
+      throw new Error(error);
+    });
+}
+
+function showErrorMessage() {
+  Notify.Notify.failure('Oops, there is no country with that name');
+}
+
+function showTooManyCountriesMessage() {
+  Notify.Notify.info(
+    'Too many matches found. Please enter a more specific name.'
+  );
+}
+
+function makeCountriesMarkup(countries) {
+  let markup = '';
+
+  countries.forEach(country => {
+    markup += `<li><img src=${country.flags.png} alt=${country.flags.alt} /> <span> ${country.name.official}</span></li>`;
+  });
+  return markup;
+}
+
+function makeCountryMarkup(countries) {
+  const languages = Object.values(countries[0].languages).join(', ');
+
+  const markup = `<p><img src=${countries[0].flags.png} alt=${countries[0].flags.alt} /> <span> ${countries[0].name.official}</span></p><p><span class="country-desc">Capital: ${countries[0].capital}</span></p><p><span class="country-desc">Population: ${countries[0].population}</span></p><p><span class="country-desc">Languages: </span>${languages}</p>`;
+
+  return markup;
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,11 @@
+const URL = 'https://restcountries.com/v3.1/name/';
+const filter = `?fields=name,capital,population,flags,languages`;
+
+export function fetchCountries(name) {
+  return fetch(`${URL}${name}${filter}`).then(r => {
+    if (!r.ok) {
+      throw new Error(r.status);
+    }
+    return r.json();
+  });
+}
